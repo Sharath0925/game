e75a59c588e06b20db482542eb4ce1b1e12d3942
Sharath0925 <143480261+Sharath0925@users.noreply.github.com>
2025-05-14 17:07:18 +0530

    aaaaaa

--- gameproject/gameproject/settings.py
+++ gameproject/gameproject/settings.py
@@ -50,8 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    
-    
 ]
 
 ROOT_URLCONF = 'gameproject.urls'
