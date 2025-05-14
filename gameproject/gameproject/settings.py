e78f7e9c7d122f7ba59b3e0f989abd148430e5f5
Sharath0925 <143480261+Sharath0925@users.noreply.github.com>
2025-05-14 16:15:33 +0530

    env fixed

--- gameproject/gameproject/settings.py
+++ gameproject/gameproject/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     
 ]
 
